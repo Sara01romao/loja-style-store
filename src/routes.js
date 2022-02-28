4745ef03485ff339ab5cbf00bc1b52f15438656c
Sara Romao <sara-romao@live.com>
2022-02-28 14:21:18 -0300

    header desktop mais rota

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 
@@ -8,6 +9,7 @@ export default function Rota(){
 
     return(
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 
