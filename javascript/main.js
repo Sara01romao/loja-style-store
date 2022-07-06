6741db3e4e366d911f60423af7e06f3469195458
Sara Romão <sara-romao@live.com>
2022-07-05 22:42:41 -0300

    open e close carrinho

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+const closeCart = document.querySelector('.close-cart');
+const openCart = document.querySelector('.open-Cart');
+const cartMenu = document.querySelector('.cartMenu');
+
+closeCart.addEventListener('click', ()=>{
+   
+    cartMenu.classList.remove('open');
+
+})
+
+openCart.addEventListener('click',()=>{
+    
+    cartMenu.classList.add('open');
+})
+
+
+console.log(closeCard)
