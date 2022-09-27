2dccfdce2edce0b2aac459180afb6631b2c76b9b
Sara Romão <sara-romao@live.com>
2022-09-26 23:48:06 -0300

    add descrição e avaliação

--- javascript/list_products.js
+++ javascript/list_products.js
@@ -2,8 +2,11 @@
    {
      id:1,
      title:'Camiseta Preta',
-     price:50,
-     img:"../img/camiseta-preta.png"
+     price:50.99,
+     image:"../img/camiseta-preta.png",
+     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+     rating: {rate: 3.9, count: 120}
+
    },
    
  ]
