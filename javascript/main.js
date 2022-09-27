6edcfa09e6ad7ef3546a68d74aa5f5e30528b38a
Sara Romão <sara-romao@live.com>
2022-09-26 23:47:26 -0300

    api e objeto

--- javascript/main.js
+++ javascript/main.js
@@ -4,19 +4,18 @@ const listProducts = document.querySelector('.list-products');
 
 const {products} = await import (`./list_products.js`);
 
-console.log(products)
 
 
-   listProducts.innerHTML = `<li>${products.map(item => item.title).join('')}</li>`
+  /*  listProducts.innerHTML = `<li>${products.map(item => item.title).join('')}</li>` */
 
 
-/* 
+
  async function api(){
      const response = await fetch('https://fakestoreapi.com/products');
      const data = await response.json();
 
      if(data){
-         
+          /* listProducts.innerHTML = `<li>${products.map(item => item.title).join('')}</li>`  */
      
          
      }
@@ -25,6 +24,6 @@ console.log(products)
 
 
  api()
- */
+
 
 
